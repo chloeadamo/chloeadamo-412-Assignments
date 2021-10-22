eb82250d5a99fce009f055f5153ca2562b4f2edc
chloeadamo <78975524+chloeadamo@users.noreply.github.com>
2021-10-22 02:47:27 -0400

    Update ps4.js

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -3,8 +3,6 @@ var router = express.Router();
 const request = require('request');
 let config = require('../config/config.js');
 
-// let appId  = '6b36a5c1c622699f22be5e946bc5be9f';
-
 /* POST request  */
 router.post('/weather', function (req, res, next) {
   return new Promise((resolve, reject) => {
